fb17e8aee20f68910ae2f7f59a77b80c4c7f65bd
Prince Billy Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2019-05-01 14:30:58 +0600

    Client project done

--- dist/config/init.php
+++
@@ -1,9 +0,0 @@
-<!-- init.php -->
-<?php 
-	require 'config.php';
-	spl_autoload_register(function($className){
-		require_once 'lib/'.str_replace('\\', '/', $className).'.php';
-	});
-
-	
- ?>
--- dist/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	session_start();
-	if(isset($_SESSION))
-		unset($_SESSION);
-	session_destroy();
-	header("Location: {$_SERVER['HTTP_REFERER']}");
- ?>
